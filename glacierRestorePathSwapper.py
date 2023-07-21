c5f53de67e4359331509393fa44a8848349d72e3
amsiner <55514849+amsiner@users.noreply.github.com>
2023-07-21 13:11:19 -0700

    Update glacierRestorePathSwapper.py

--- glacierRestorePathSwapper.py
+++ glacierRestorePathSwapper.py
@@ -25,20 +25,19 @@ def select_and_repath_readgeo_nodes():
 #UNTESTED - NO CURRENT RESTORE JOBS WITH CAMERAS TO TEST            
 '''
 def select_and_repath_camera_nodes():
-    camera_nodes = [node for node in nuke.allNodes() if node.Class() == 'Camera']
+    camera_nodes = [node for node in nuke.allNodes() if node.Class().startswith('Camera')]
     for node in camera_nodes:
         #CHECKING FIRST TO SEE IF THE CAMERA NODE IS READING FROM FILE, OTHERWISE CAN CRASH 
-        readFromFile = node.knob('read_from_file_link')
-            if readFromFile = '0.0':
-                pass
-            else:
-                file_path = node['file_link'].getValue()
-                split_path = file_path.split('/')
-                if len(split_path) >= 3:
-                    split_path.insert(2, 'jobs')
-                    split_path.insert(3, 'glacier-restore')
-                    new_path = '/'.join(split_path)
-                    node['file'].setValue(new_path)
+        if node.knob('read_from_file').getValue() == '0.0':
+            pass
+        else:
+            file_path = node['filek'].getValue()
+            split_path = file_path.split('/')
+            if len(split_path) >= 3:
+                split_path.insert(2, 'jobs')
+                split_path.insert(3, 'glacier-restore')
+                new_path = '/'.join(split_path)
+                node['file'].setValue(new_path)
 '''
 
 select_and_repath_read_nodes()
